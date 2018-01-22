1c9eacb3adc1fd3ababbcba8ff04b93ddcf6bbd0
sachinksalim <sachink@iitk.ac.in>
2018-01-22 21:24:02 +0530

    added occurrence calculator

--- asgn1/src/test.py
+++ asgn1/src/test.py
@@ -1,17 +1,25 @@
 from lex import *
-import collections
 
 data = "2+6*85+1"
 
 # Give the lexer some input
 lexer.input(data)
 
-lexer.sort(key=lambda x: x.lexpos, reverse=False)
-#counter=collections.Counter(lexer.type)
-#print(counter)
+tokenDict = {}
 
 # Tokenize
 print("{0:^16} {1:^16} {2:^8}".format("Token", "Occurrences", "Lexemes"))
 print("{0:^16} {1:^16} {2:^8}".format("-----", "-----------", "-------"))
 for tok in lexer:
-    print("{0:^16} {1:^16} {2:^8}".format(tok.type, tok.lexpos, tok.value))
+    if tok.type in tokenDict:
+        tokenDict[tok.type].append(tok.value)
+    else:
+        tokenDict.update({tok.type: [tok.value]})
+    #print("{0:^16} {1:^16} {2:^8}".format(tok.type, tok.lexpos, tok.value))
+
+#print(tokenDict)
+
+for token, lexeme in tokenDict.items():
+    print("{0:^16} {1:^16} {2:^8}".format(token, len(lexeme), lexeme[0]))
+    for lex in lexeme[1:]:
+        print("{0:^75}".format(lex))
